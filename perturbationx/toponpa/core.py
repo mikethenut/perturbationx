fix: Remove unnecessary computation of inverse.

perturbationx/toponpa/core.py
@@ -19,7 +19,7 @@ def coefficient_inference(lap_b, lap_c, boundary_coefficients: np.ndarray):
         lap_c = lap_c.todense()
 
     edge_constraints = - lap_b @ boundary_coefficients
-    return la.inv(lap_c) @ edge_constraints
+    return la.solve(lap_c, edge_constraints)
 
 
 def perturbation_amplitude(lap_q: np.ndarray, core_coefficients: np.ndarray, core_edge_count: int):

perturbationx/toponpa/statistics/permutation_tests.py
@@ -9,12 +9,12 @@ def test_boundary_permutations(lap_b, lap_c, lap_q, boundary_coefficients: np.nd
                                core_edge_count: int, permutation_rate=1., iterations=500, seed=None):
     if issparse(lap_c):
         lap_c = lap_c.todense()
-    lap_c_inv = la.inv(lap_c)
-    inference_matrix = - lap_c_inv @ lap_b if not issparse(lap_b) else None
+    inference_matrix = - la.solve(lap_c, lap_b) if not issparse(lap_b) else None
 
     rng = np.random.default_rng(seed)
     permutation_count = np.ceil(permutation_rate * boundary_coefficients.shape[0]).astype(int)
     distribution = []
+
     for p in range(iterations):
         permuted_idx = rng.choice(boundary_coefficients.shape[0], size=permutation_count, replace=False)
         permuted_boundary = boundary_coefficients.copy()
@@ -22,7 +22,7 @@ def test_boundary_permutations(lap_b, lap_c, lap_q, boundary_coefficients: np.nd
             rng.permutation(permuted_boundary[permuted_idx])
 
         if issparse(lap_b):  # We do not precompute the inference matrix for sparse matrices
-            permuted_core = - lap_c_inv @ (lap_b @ permuted_boundary)
+            permuted_core = - la.solve(lap_c, lap_b @ permuted_boundary)
         else:
             permuted_core = inference_matrix @ permuted_boundary
 
@@ -43,12 +43,15 @@ def test_core_permutations(adj_perms, boundary_coefficients: np.ndarray, lap_b: 
 
     for adj_c_perm in adj_perms:
         lap_c_perm, lap_q_perm = generate_core_laplacians(lap_b, adj_c_perm, exact_boundary_outdegree)
+
+        if issparse(lap_c_perm):
+            lap_c_perm = lap_c_perm.todense()
+        core_coefficients = la.solve(lap_c_perm, edge_constraints)
+
         if full_permutations:
             lap_q = lap_q_perm
-
-        lap_c_inv = la.inv(lap_c_perm.todense()) if issparse(lap_c_perm) else la.inv(lap_c_perm)
-        core_coefficients = lap_c_inv @ edge_constraints
         sample_perturbation = core_coefficients.T @ lap_q @ core_coefficients
+
         distribution.append(sample_perturbation / core_edge_count)
 
     return distribution