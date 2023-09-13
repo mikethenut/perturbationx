fix: Adjust sparse computation.

perturbationx/toponpa/matrices.py
@@ -41,7 +41,12 @@ def generate_boundary_laplacian(adj_b, boundary_edge_minimum=6):
     if issparse(adj_b):
         # Clip edges where boundary outdegree is below threshold
         row_nonzero_counts = np.bincount(adj_b.nonzero()[0], minlength=adj_b.shape[0])
-        adj_b[row_nonzero_counts < boundary_edge_minimum, :] = 0
+        clipping_mask = row_nonzero_counts < boundary_edge_minimum
+        clipping_mask *= row_nonzero_counts > 0
+
+        clip_row_idx = [idx for idx, clip_row in enumerate(clipping_mask) if clip_row] + [0]
+        for idx in clip_row_idx:
+            adj_b[idx, :] = 0
 
         row_sums = abs(adj_b).sum(axis=1)
         row_sums[row_sums == 0] = 1

perturbationx/toponpa/permutation/adjacency_permutation.py
@@ -63,16 +63,25 @@ def adjacency_permutation_k2(adj: np.ndarray, iterations=500, permutation_rate=1
 
         permuted_edge_weights = edge_weights.copy()
         permuted_edge_weights[permuted_idx] = rng.permutation(permuted_edge_weights[permuted_idx])
-        permuted_edge_weights = list(permuted_edge_weights)
 
-        random_adj = np.zeros(adj.shape)
-        for (src, trg), weight in zip(permuted_stubs, permuted_edge_weights):
-            if src != trg:
-                random_adj[src, trg] += weight
-                random_adj[trg, src] += weight
+        if issparse(adj):
+            random_adj = lil_array(adj.shape)
+        else:
+            random_adj = np.zeros(adj.shape)
+
+        src_indices, trg_indices = zip(*permuted_stubs)
+        mask = src_indices != trg_indices
+
+        src_indices = np.array(src_indices)[mask]
+        trg_indices = np.array(trg_indices)[mask]
+        permuted_edge_weights = permuted_edge_weights[mask]
+
+        random_adj[src_indices, trg_indices] = permuted_edge_weights
+        random_adj[trg_indices, src_indices] += permuted_edge_weights
 
         if ensure_connectedness:
             connect_adjacency_components(random_adj, weights=edge_weights, seed=seed)
+
         permuted.append(random_adj)
 
     return permuted

perturbationx/toponpa/statistics/permutation_tests.py
@@ -22,7 +22,7 @@ def test_boundary_permutations(lap_b, lap_c, lap_q, boundary_coefficients: np.nd
             rng.permutation(permuted_boundary[permuted_idx])
 
         if issparse(lap_b):  # We do not precompute the inference matrix for sparse matrices
-            permuted_core = - lap_c_inv @ lap_b @ permuted_boundary
+            permuted_core = - lap_c_inv @ (lap_b @ permuted_boundary)
         else:
             permuted_core = inference_matrix @ permuted_boundary
 